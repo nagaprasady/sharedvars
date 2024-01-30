45c5d89f1373a54b939392ef5ccdfeab1cd7e9e2
nagaprasady <46271227+nagaprasady@users.noreply.github.com>
2024-01-30 23:52:13 +0000

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -6,7 +6,11 @@ def mavenbuild()
 {
   "sh mvn package"
 }
-def tomcatdeploy(jobname,ip,context)
-{
-  sh "scp  /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${context}.war"
-}
+//def runSelenium(jobname)
+//{
+//sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
+//}
+//def tomcatdeploy(jobname,ip,context)
+//{
+  //sh "scp  /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${context}.war"//
+//}
