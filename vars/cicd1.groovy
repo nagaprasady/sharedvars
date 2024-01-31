dad42c813e72f1788e863b46379e6882973b2f76
nagaprasady <46271227+nagaprasady@users.noreply.github.com>
2024-01-31 12:39:26 +0000

    Update cicd1.groovy

--- vars/cicd1.groovy
+++ vars/cicd1.groovy
@@ -10,4 +10,7 @@ def tomcatdeploy(jobname,ip,context)
 { 
  sh  "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${context}.war"
 }
- 
+def runselenium(jobname)
+{
+sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
+}
