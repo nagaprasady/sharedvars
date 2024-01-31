1653488c6d1641b5891930cc160d0304c0013992
nagaprasady <46271227+nagaprasady@users.noreply.github.com>
2024-01-31 11:49:47 +0000

    Update cicd1.groovy

--- vars/cicd1.groovy
+++ vars/cicd1.groovy
@@ -6,4 +6,7 @@ def mavenbuild()
 {
   sh "mvn package"
 }
-
+def tomcatdeploy(jobname,ip,context)
+{ 
+  scp "/var/lib/jenkins/workspace/{jobname}/webapp/target/webapp.war ubuntu@{ip}:/var/lib/tomcat9/webapps/${context}.war/"
+}
